8cb6f5c9dc8e52c22abbe9a36fc0adede99179d3
trandiepp2105 <tranvandiepp2105@gmail.com>
2024-01-14 01:10:39 +0700

    config database

--- app/app/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import SimpleTestCase
-from app import calc
-class AdditionClass(SimpleTestCase):
-    def test_add_number(self):
-        """ Test the addition of two positive numbers"""
-        rel = calc.add(2,3)
-        self.assertEqual(rel, 7)
